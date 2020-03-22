f1caa73e32467f72f32483bbf64794d3faaf0aff
Abhi Nandam <anandam@moneylion.com>
2020-03-22 19:17:07 -0400

    fetches leaderboard stats

---
+++ leaderboard.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from time import sleep
+from secrets import NY_TIMES_CREDENTIALS
+
+POST_LOGIN_URL = 'https://myaccount.nytimes.com/auth/login/'
+LEADERBOARD_URL = 'https://www.nytimes.com/puzzles/leaderboards'
+
+LEADERBOARD_ROW_DELIMITER = '•'
+
+
+class NYTLeaderboard(object):
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get(POST_LOGIN_URL)
+        sleep(2)
+
+        google_auth_button = self.driver.find_element_by_xpath('// *[ @ id = "js-google-oauth-login"]')
+        google_auth_button.click()
+        sleep(2)
+
+        # switch to login popup
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="identifierId"]')
+        email_in.send_keys(NY_TIMES_CREDENTIALS['username'])
+
+        next_button = self.driver.find_element_by_xpath('//*[@id="identifierNext"]')
+        next_button.click()
+        sleep(2)
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
+        pw_in.send_keys(NY_TIMES_CREDENTIALS['password'])
+
+        next_button = self.driver.find_element_by_xpath('//*[@id="passwordNext"]')
+        next_button.click()
+
+        self.driver.switch_to.window(base_window)
+
+    def fetch_leaderboard(self):
+        self.driver.get(LEADERBOARD_URL)
+        leaderboard_text = self.driver.find_element_by_xpath('//*[@id="lbd-root"]/div/div[2]').text
+        leaderboard_details = self.parse_leaderboard_text(leaderboard_text)
+        for solver in leaderboard_details:
+            print(solver.name, solver.time)
+
+    def parse_leaderboard_text(self, leaderboard_text):
+        rows = leaderboard_text.split(LEADERBOARD_ROW_DELIMITER)
+        leaderboard_details = []
+        for row in rows:
+            stats = row.split('\n')
+            leaderboard_details.append(SolverStats(stats[0], stats[1], stats[2]))
+        return leaderboard_details
+
+
+class SolverStats(object):
+
+    def __init__(self, rank, name, time):
+        self.rank = rank
+        self.name = name
+        self.time = time
